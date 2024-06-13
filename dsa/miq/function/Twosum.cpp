d8392c3ff9affc4594547a5cd8d7c069ac95c2c9
= <=>
2024-06-13 22:13:29 +0530

    sum of pair

--- dsa/miq/function/Twosum.cpp
+++ dsa/miq/function/Twosum.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 int fourSum(int nums[], int size, int target)
 {
@@ -25,6 +25,28 @@ int fourSum(int nums[], int size, int target)
 }
 int main()
 {
+    vector<int> arr{10, 20, 30, 40, 50, 60, 70};
+    int target = 100;
+    for (int i = 0; i < arr.size(); i++)
 
+    {
+        int element1 = arr[i];
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            int element2 = arr[j];
+            for (int k = j + 1; k < arr.size(); k++)
+            {
+                int element3 = arr[k];
+                for (int l = k + 1; l < arr.size(); l++)
+                {
+                    int element4 = arr[l];
+                    if (element1 + element2 + element3 + element4 == target)
+                    {
+                        cout << "Pair found:" << element1 << " " << element2 << " " << element3 << " " << element4 << "  ";
+                    }
+                }
+            }
+        }
+    }
     return 0;
 }
