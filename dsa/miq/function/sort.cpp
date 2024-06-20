4685026836247ff1eb973887c1844177da73d795
= <mayankrathore9897@gmail.com>
2024-06-20 06:45:46 +0530

    Dutch National Flag

--- dsa/miq/function/sort.cpp
+++ dsa/miq/function/sort.cpp
@@ -35,38 +35,60 @@ void sorted(vector<int> &nums)
     //     nums[i] = 2;
     //     i++;
     // }
-    cout << "After sorting" << endl;
+    // cout << "After sorting" << endl;
+    // int l = 0;
+    // int n = 0;
+    // int r = nums.size() - 1;
+    // while (n <= r)
+    // {
+    //     /* code */
+
+    //     if (nums[n] == 0)
+    //     {
+
+    //         swap(nums[l], nums[n]);
+    //         l++;
+    //         n++;
+    //     }
+    //     else if (nums[n] == 1)
+    //     {
+
+    //         n++;
+    //     }
+    //     else
+    //     {
+    //         swap(nums[r], nums[n]);
+
+    //         r--;
+    //     }
+    // }
+    // T.C=O(N)
+    // S.C=O(1)
+    // Dutch NAtional algorithm;
     int l = 0;
-    int n = 0;
-    int r = nums.size() - 1;
-    while (n <= r)
+    int h = nums.size() - 1;
+    while (l < h)
     {
-        /* code */
-
-        if (nums[n] == 0)
+        if (nums[l] < 0)
         {
-
-            swap(nums[l], nums[n]);
             l++;
-            n++;
         }
-        else if (nums[n] == 1)
+        else if (nums[h] > 0)
         {
-
-            n++;
+            h--;
         }
         else
         {
-            swap(nums[r], nums[n]);
-
-            r--;
+            swap(nums[l], nums[h]);
         }
     }
 };
 
 int main()
 {
-    vector<int> nums = {0, 1, 0, 2, 1, 0, 2, 0};
+    // vector<int> nums = {0, 1, 0, 2, 1, 0, 2, 0};
+    // DutchNational Flag algorithm (2 pointer approach)
+    vector<int> nums = {2, 3, 4, -1, 4, -2, 4, -5, 8, -7};
     // sorted(nums);
     sorted(nums);
     for (int number : nums)
