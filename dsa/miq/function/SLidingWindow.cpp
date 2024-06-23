4a694d79cd213a7b3ee42c1e9e285336ba8cd821
= <mayankrathore9897@gmail.com>
2024-06-23 19:56:00 +0530

    Maximum sum subarray by using sliding window technique

--- dsa/miq/function/SLidingWindow.cpp
+++ dsa/miq/function/SLidingWindow.cpp
@@ -36,13 +36,39 @@ public:
     }
 };
 // I M providing sliding problem question
+int MaximumSumsubarray(vector<int> &nums, int k)
+{
+    int sum = 0;
+    int j = 0;
+    int i = 0;
+    int max_sum = 0;
+    while (j < nums.size())
+    {
+        sum += nums[j];
+        if (j - i + 1 < k)
+        {
+
+            j++;
+        }
+        else if (j - i + 1 == k)
+        {
+            max_sum = max(max_sum, sum);
+            sum = sum - nums[i];
+            i++;
+            j++;
+        }
+        }
+    return max_sum;
+}
 int main()
 {
     Solution obj1;
     vector<int> customers = {1, 0, 1, 2, 1, 1, 7, 5};
     vector<int> grumpy = {0, 1, 0, 1, 0, 1, 0, 1};
+    vector<int> arr = {2, 5, 1, 8, 2, 9};
     int m = 3;
-    cout << obj1.maxSatisfied(customers, grumpy, m);
+    // cout << obj1.maxSatisfied(customers, grumpy, m);
+    cout << MaximumSumsubarray(arr, m);
 
     return 0;
 }
