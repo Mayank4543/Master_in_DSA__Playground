efc7ea0c349ff3d20027e67b095cda01d0c79aab
= <mayankrathore9897@gmail.com>
2024-06-25 22:28:49 +0530

    Binary tree

---
+++ dsa/miq/function/tree/Binarytree.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+node *buildTree(node *root)
+{
+    cout << "Enter the data:" << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+    if (data == -1)
+    {
+        return NULL;
+    }
+    cout << "Enter the data inserting in left child" << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter the data inserting in right child" << endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+void inorder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+int main()
+{
+    node *root = NULL;
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    root = buildTree(root);
+    cout << "Inorder Traversal:";
+    inorder(root);
+    return 0;
+}
